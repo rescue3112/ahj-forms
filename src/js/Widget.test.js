f7ccde90ee9cc34bfc187bae8daaa7ba3c4ec07e
AppVeyor <ci@appveyor.com>
2023-07-27 01:24:04 +0700

    1commit

---
+++ src/js/Widget.test.js
@@ -0,0 +1,11 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import Widget from './Widget';
+
+test('whether class can actually create widget', () => {
+  new Widget();
+  const widgetWindow = document.querySelector('.widget-window');
+  expect(widgetWindow).toBeTruthy();
+});
---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+import Widget from './Widget';
+import Popuper from './Popuper';
+
+new Widget();
+new Popuper();
